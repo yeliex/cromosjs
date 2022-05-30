ce42895e471e2d2c2882b35b18d9c1e5dee8746c
yeliex <yeliex@yeliex.com>
2022-05-30 10:54:31 +0800

    WIP.

---
+++ packages/runtime/src/index.ts
@@ -0,0 +1,12 @@
+const RuntimeComponent: WechatMiniprogram.Component.Constructor = (options) => {
+
+};
+
+const RuntimePage: typeof RuntimeComponent = (options) => {
+
+};
+
+export {
+    RuntimeComponent as Component,
+    RuntimePage as Page,
+};
